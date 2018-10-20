fc6a0dccba6c1d306b83b8ad5a52132c0d8c17a7
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-10-20 17:58:35 +0100

    chore(redux): redux set up
    add actions folder
    add course actions file
    add create course action
    add course reducer
    add root reducer
    
    [Starts #161362720]

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,13 +2,44 @@ import React, { Component } from 'react'
 
 
 export class CoursesPage extends Component {
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      course: {title: ''}
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave(){
+    alert('s');
+  }
+  
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input 
+          type= "text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        <input 
+          type= "submit"
+          onClick={this.onClickSave}
+          value="Save"
+        />
       </div>
-    )
+    );
   }
 }
 
-export default CoursesPage
+export default CoursesPage;
