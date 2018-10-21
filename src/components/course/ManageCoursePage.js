d6592486210743f499a99fc45744b34e43e8d7e2
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-10-21 22:32:12 +0100

    feat(course): save course
    - add create course functionalities
    - add update course functionalites
    
    [Finishes #161367575]

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -9,9 +9,30 @@ class ManageCoursePage extends Component {
     super(props, context);
     
     this.state = {
-      course: Object.assign({}, this.props.course),
+      course: Object.assign({}, props.course),
       errors: {}
     };
+
+    this.updateCourseState = this.updateCourseState.bind(this);
+    this.saveCourse = this.saveCourse.bind(this);
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id != nextProps.course.id){
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
+  updateCourseState(event) {
+    let course = this.state.course;
+    course = Object.assign({}, course, { [event.target.name]: event.target.value});
+    return this.setState({course});
+  }
+
+  saveCourse(event) {
+    event.preventDefault();
+    this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
   render() {
     return (
@@ -19,6 +40,8 @@ class ManageCoursePage extends Component {
           course={this.state.course}
           errors={this.state.errors}
           allAuthor={this.props.authors}
+          onChange={this.updateCourseState}
+          onSave={this.saveCourse}
         />
     );
   }
@@ -26,10 +49,21 @@ class ManageCoursePage extends Component {
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired
 };
 
-const mapStateToProps = (state) => {
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object
+};
+
+const getCourseById = (courses, id) => {
+  const course = courses.filter(course => course.id === id);
+  return course ? course[0] : null;
+};
+
+const mapStateToProps = (state, ownProps) => {
+  let courseId = ownProps.params.id;
   let course = {
     id: '',
     watchHref: '',
@@ -38,6 +72,10 @@ const mapStateToProps = (state) => {
     length: '',
     category: ''
   };
+
+  if (courseId && state.courses.length > 0){
+    course = getCourseById(state.courses, courseId);
+  }
 
   const transformAuthorsForDropdown = state.authors.map(author => ({
     value: author.id,
